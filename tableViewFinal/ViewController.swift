bc999c30457acf5077b4c0dce05877f23a3dd94d
Sidhant <sidhantm.ic.17@nsit.net.in>
2021-01-19 20:50:56 +0530

    Initial Commit

---
+++ tableViewFinal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableViewFinal
+//
+//  Created by Sidhant Madan on 19/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
