6db072c785ab648031aaa15c469c4243fdb90553
Sidhant <sidhantm.ic.17@nsit.net.in>
2021-01-27 18:48:02 +0530

    Implemented auto layout

---
+++ tableViewFinal/CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  tableViewFinal
+//
+//  Created by Sidhant Madan on 22/01/21.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
